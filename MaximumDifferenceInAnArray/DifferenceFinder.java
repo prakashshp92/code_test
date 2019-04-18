bc6667143095539f2f2542916ac2e2962baaabc0
prakash premkumar <prakash.p.2292@gmail.com>
2019-04-18 12:08:17 +0530

    removed package names

--- MaximumDifferenceInAnArray/DifferenceFinder.java
+++ MaximumDifferenceInAnArray/DifferenceFinder.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package differencefinder;
+
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
